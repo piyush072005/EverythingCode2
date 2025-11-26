76ff98e84882abc8caa1adb9a75de87be6b686d8
piyush072005 <piyushbatra2005@gmail.com>
2025-11-26 21:12:35 +0530

    special array

---
+++ python/specialarray.py
@@ -0,0 +1,24 @@
+#zero Array
+import numpy as np
+ar_zero=np.zeros(4)
+#2Darray
+ar_zero1=np.zeros((3,4))
+print(ar_zero)
+print(ar_zero1)
+#one array
+ar_zero2=np.ones(4)
+print(ar_zero2)
+zero=np.ones((2,2))
+print(zero)
+#empty array
+d=np.empty(4)
+print(d)
+#range
+d1=np.arange(4)
+print(d1)
+#diagonal array element
+dia_1=np.eye(3,5)
+print(dia_1)
+#spl type array with space
+lin_1=np.linspace(1,10,num=5)
+print(lin_1)
