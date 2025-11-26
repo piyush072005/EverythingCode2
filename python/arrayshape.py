a7e2243cbbb104178d53854e4f6e078f9d45eb6f
piyush072005 <piyushbatra2005@gmail.com>
2025-11-26 21:08:21 +0530

    dot+shape+type

---
+++ python/arrayshape.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a=np.array([[1, 2, 3],[2,4,6]])
+b=np.array([[4, 5, 6],[1,2,3]])
+
+print("Shape of a:", a.shape)
+print("Shape of b:", b.shape)
---
+++ python/arraytype.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a=np.array([1, 2, 3])
+b=np.array([4, 5, 6])
+c=np.add(a, b)
+print(c.dtype)
+print(type(c))
+#class type and data type of the resultant array after addition
