c8aa61c6364d0b718105e5c016846d387764540a
piyush072005 <piyushbatra2005@gmail.com>
2025-12-05 22:45:02 +0530

    hollowpat

--- python/hollowpatt.py
+++ python/hollowpatt.py
@@ -3,9 +3,9 @@ cols = 10
 
 for i in range(rows):
     if i == 0:
-        print("*")
+        print("*" * cols)
     elif i == rows - 1:
         print("*" * cols)
     else:
-        print("" + " " * i + "")"*" + (" " * (cols - 2) + "*")
+        print("*" + " " * (cols - 2) + "*")
     print("")
